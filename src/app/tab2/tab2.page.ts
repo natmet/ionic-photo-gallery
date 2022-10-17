e07ba8ec2b4d721cfebadc5db1921b69bca03244
Natanael Sanchez <natanaelalbertomed@gmail.com>
2022-10-16 22:49:00 -0400

    Final Commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserPhoto } from 'src/core/models/userPhoto.model';
+import { PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  constructor(public photoService: PhotoService,
+    public actionSheetController: ActionSheetController) {}
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  addPhotoToGallery(){
+    this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number){
+
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: ()=> {
+          this.photoService.deletePicture(photo, position)
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {}
+      }]
+    });
+    await actionSheet.present();
+
+  }
 
 }
